27944118041f5945d87c17f2bfd6ac0c9c837ebb
lavish <lavish99neema@gmail.com>
2021-05-03 11:59:14 +0530

    first commit

---
+++ src/test/java/qaclickacademy/Mavenjava/AppiumTest.java
@@ -0,0 +1,16 @@
+package qaclickacademy.Mavenjava;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+
+	@Test
+	public void NativeAndroidApps() {
+		System.out.println("NativeAndroidApps");
+	}
+	
+	@Test
+	public void IosApps() {
+		System.out.println("IosApps");
+	}
+}
---
+++ src/test/java/qaclickacademy/Mavenjava/RestApiTest.java
@@ -0,0 +1,17 @@
+package qaclickacademy.Mavenjava;
+
+import org.testng.annotations.Test;
+
+public class RestApiTest {
+
+      @Test
+	public void Postjira() {
+		System.out.println("postjira");
+		
+	}
+	@Test
+	public void deleteTwitter() {
+		System.out.println("deleteTwitter");
+	}
+}
+
